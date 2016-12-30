a223d13943dd64966acf47aa0fe380094b48aa0e
cortesarts <dennis.cortes@satchelhealth.com>
2016-12-30 16:35:45 -0600

    Adds Sun and Moon components

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 
 import Header from '../Header';
+import Sun from '../Sun';
+import Moon from '../Moon';
 
 class Home extends Component {
   render() {
     return (
-      <div id="Home">
+      <div id="home">
         <Header />
-        <h1>Home</h1>
+        <div id="home-content" className="pv5-ns pv4 mw9 center w-90-ns w-100">
+          <div className="bg-green">
+            <div className="current cf">
+              <Sun />
+              <Moon />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/components/Sun/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Sun extends Component {
+  render() {
+    return (
+      <div id="sun">
+        <h1>Sun</h1>
+      </div>
+    );
+  }
+}
+
+export default Sun;
