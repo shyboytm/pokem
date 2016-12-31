4dc49ffd3cc2fbc68f655aa0f1aa0ed0e4682bb9
cortesarts <dennis.cortes@satchelhealth.com>
2016-12-30 19:58:46 -0600

    Updating header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,12 +4,12 @@ class Header extends Component {
   render() {
     return (
       <nav className="pa3 pa4-ns bg-red">
-        <a className="ttu tracked link white dim black b f2-ns f3 tc db mb3 mb4-ns" href="#" title="Home">Pokerus</a>
+        <a className="ttu tracked link white dim black b f2-ns f3 tc db mb3 mb4-ns" href="#" title="Home">Pokém</a>
         <div className="tc pb3">
-          <a className="link hover-white white-70 f6 f5-ns dib mr3" href="#" title="Home">Home</a>
-          <a className="link hover-white white-70 f6 f5-ns dib mr3" href="#" title="About">About</a>
+          <span className="white measure">Personal Pokemon Team and Pokedex Tracker of <a className="link hover-white white-70 f6 f5-ns dib mr3" href="http://cortes.us" title="Dennis Cortés">Dennis Cortés</a></span>
+          {/* <a className="link hover-white white-70 f6 f5-ns dib mr3" href="#" title="About">About</a>
           <a className="link hover-white white-70 f6 f5-ns dib mr3" href="#" title="Store">Store</a>
-          <a className="link hover-white white-70 f6 f5-ns dib" href="#" title="Contact">Contact</a>
+          <a className="link hover-white white-70 f6 f5-ns dib" href="#" title="Contact">Contact</a> */}
         </div>
       </nav>
     );
